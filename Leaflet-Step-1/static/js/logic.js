13b3c53edf55ad2c5a746b0b1ed9d796c32a9033
Mary Bellavia <sauerwein.mary@gmail.com>
2021-03-11 19:43:29 -0600

    map with markers working

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -71,6 +71,7 @@ function createFeatures(earthquakeData) {
 // d3.json(queryUrl, function(data) {
 //   // Once we get a response, send the data.features object to the createFeatures function
 //   createFeatures(data.features);
+//   console.log(data);
 // });
 
 // function createFeatures(earthquakeData) {
@@ -93,7 +94,7 @@ function createFeatures(earthquakeData) {
 // }
 
 // function createMap(earthquakes) {
-
+//     // console.log(earthquakes);
 //   // Define streetmap and darkmap layers
 //   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 //     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
